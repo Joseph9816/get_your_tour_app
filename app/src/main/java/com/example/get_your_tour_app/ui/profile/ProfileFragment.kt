7d119e7adc3d44d3e3da6e857901b9b3beec0573
carlosDev22 <71095613+carlosDev22@users.noreply.github.com>
2021-05-01 09:58:18 -0600

    commit error bending

--- app/src/main/java/com/example/get_your_tour_app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/get_your_tour_app/ui/profile/ProfileFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.get_your_tour_app.R
+import com.example.get_your_tour_app.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +27,18 @@ class ProfileFragment : Fragment() {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_profile)
+        val button: Button = root.findViewById(R.id.button3)
         profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            button.setOnClickListener{
+                it.findNavController().navigate(R.id.action_navigation_profile_to_navigation_register)
+            }
         })
-        return root
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/get_your_tour_app/ui/register/RegisterFragment.kt
@@ -0,0 +1,5 @@
+package com.example.get_your_tour_app.ui.register
+
+class RegisterFragment {
+
+}
