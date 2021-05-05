a252a038e76e7b7926266913b39a14cb7acd2af2
carlosDev22 <71095613+carlosDev22@users.noreply.github.com>
2021-05-05 01:19:36 -0600

    validate password strong

--- app/src/main/java/com/example/get_your_tour_app/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/get_your_tour_app/ui/register/RegisterFragment.kt
@@ -1,6 +1,7 @@
 package com.example.get_your_tour_app.ui.register
 
 import android.os.Bundle
+import android.text.AutoText
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -9,12 +10,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.get_your_tour_app.databinding.FragmentRegisterBinding
+import java.util.regex.Pattern
+
 private const val TAG = "noOverride"
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
     private var emailStatus = false
+    private var pass1 = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,7 +51,32 @@ class RegisterFragment : Fragment() {
             }
 
         })
+        binding.editTextTextPassword.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(passValidate(binding.editTextTextPassword.text.toString())){
+                    pass1 = true
+                }else{
+                    pass1 = false
+                    binding.editTextTextPassword.setError("the password should have 1 Upercase, 1 undercase, one number and one character")
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
+    private fun passValidate(text: String?): Boolean{
+        var p = Pattern.compile("^(?=.*\\d)(?=.*[\\u0021-\\u002b\\u003c-\\u0040])(?=.*[A-Z])(?=.*[a-z])\\S{8,16}\$")
+        var m = p.matcher(text)
+        return m.matches()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
